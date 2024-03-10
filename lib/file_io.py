75bbfbd7b6b6db8d5046c3833fc448b20377f031
mujahidina <mujahidabdi5070@gmail.com>
2024-03-10 17:14:15 +0300

    completed assignment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,13 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f'{file_name}.txt', mode='w', encoding='utf-8') as f:
+        f.write(f'{file_content}')
 
 def append_file(file_name, append_content):
-    pass
+     with open(f'{file_name}.txt', mode='a', encoding='utf-8') as f:
+        f.write(f'{append_content}')
+
 
 def read_file(file_name):
-    pass
+    with open(f'{file_name}.txt', mode="r", encoding='utf-8') as f:       
+        content = f.read()
+        return content
